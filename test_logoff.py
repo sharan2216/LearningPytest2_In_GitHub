20fba287fc06f31643231496da511d7308d19f97
shashi <sharan88btm@gmail.com>
2023-10-19 15:57:39 +0530

    move files from local to github

---
+++ test_logoff.py
@@ -0,0 +1,18 @@
+# some lines1 from sdet2
+# some lines2 from sdet2
+# some lines3 from sdet2
+
+
+
+def test_login():
+    print("Login successful")
+
+
+def test_logoff():
+    print("Logoff successful")
+
+def test_calcilation():
+    assert 2+2==4
+
+
+
---
+++ test_sdet1.py
@@ -0,0 +1,8 @@
+# some lines from sdet1
+# some lines from branch
+# some lines from branch
+# some lines from branch
+
+
+def method2():
+    print("feature_abc branc added new method")
